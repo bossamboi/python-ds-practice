1f062d129da8847fdc58c94560c8b050ffb5d61d
Eric Sohng <ericsohng@gmail.com>
2022-03-22 12:19:33 -0700

    complete problems 16 and 17

--- 17_mode.py
+++ 17_mode.py
@@ -11,3 +11,12 @@ def mode(nums):
         >>> mode([2, 2, 3, 3, 2])
         2
     """
+    highest_count = 0
+    most_common = nums[0]
+    for num in nums:
+        if nums.count(num) > highest_count:
+            highest_count = nums.count(num)
+            most_common = num
+
+    return most_common
+    
